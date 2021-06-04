b0d1a7e020e9a8fd7bd7b7fb921a2d0c94d0e9b6
Vince Geglia <vince@trifectaperformance.com>
2021-06-04 06:20:46 -0700

    Initial commit

---
+++ SmartAssembly/SingleExe/ConsoleTestApp/ConsoleTestApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = new MySecretClassLib.SecretClassLib();
+            t.ToString();
+            Console.WriteLine($"{t}");
+            Console.WriteLine($"{t.MySecretMethod()}");
+        }
+    }
+}
---
+++ SmartAssembly/SingleExe/ConsoleTestApp/MySecretClassLib/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MySecretClassLib
+{
+    public class SecretClassLib
+    {
+        private static readonly string MySecretString = "I am supposed to be an unreadable, obfuscated string.";
+
+        public string MySecretMethod() => MySecretString;
+    }
+}
